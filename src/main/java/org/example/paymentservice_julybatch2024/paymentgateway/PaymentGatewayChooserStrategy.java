95183598e61b25d95522a0c63f83fcdd729236b3
khannaanurag1 <khannaanurag1@gmail.com>
2024-09-20 23:29:45 +0530

    Stripe

--- src/main/java/org/example/paymentservice_julybatch2024/paymentgateway/PaymentGatewayChooserStrategy.java
+++ src/main/java/org/example/paymentservice_julybatch2024/paymentgateway/PaymentGatewayChooserStrategy.java
@@ -9,7 +9,11 @@ public class PaymentGatewayChooserStrategy {
     @Autowired
     private RazorpayPaymentGateway razorpayPaymentGateway;
 
+    @Autowired
+    private StripePaymentGateway stripePaymentGateway;
+
     public IPaymentGateway getBestPaymentGateway() {
-       return razorpayPaymentGateway;
+       //return razorpayPaymentGateway;
+        return stripePaymentGateway;
     }
 }
